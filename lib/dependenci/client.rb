ac0b4507ffeadbea0bb698f0c264307dcb56462c
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-05-09 16:33:50 -0500

    Client code formatting and tweaks

--- lib/dependenci/client.rb
+++ lib/dependenci/client.rb
@@ -1,24 +1,22 @@
-require 'faraday'
-require 'json'
+require "faraday"
+require "json"
 
 module Dependenci
   class Client
-    API_BASE = 'http://dependenci.com'
+    API_BASE = "http://dependenci.com"
 
-    def rubygems(opts={})
-      send_request('/api/rubygems', opts)
+    def rubygems(opts = {})
+      send_request("/api/rubygems", opts)
     end
 
-    def npm(opts={})
-      send_request('/api/npm', opts)
+    def npm(opts = {})
+      send_request("/api/npm", opts)
     end
 
     private
 
-    def send_request(path, params={})
-      url  = "#{API_BASE}#{path}"
-      resp = Faraday.post(url, params)
-
+    def send_request(path, params = {})
+      resp = Faraday.post("#{API_BASE}#{path}", params)
       JSON.parse(resp.body)
     end
   end
